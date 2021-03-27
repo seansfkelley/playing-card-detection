6e66cd035d1b2da78cbe396f0463623cb72669ed
Sean Kelley <seansfkelley@gmail.com>
2021-03-27 12:09:09 -1000

    Placeholders for the next step.

---
+++ TODO.md
@@ -0,0 +1,5 @@
+- recombine image and video extraction into one file
+- update readme and test cases
+- write scene generator that specializes in
+  - fanned cards
+  - cards randomly assorted
---
+++ card_generator/generate_scene.py
@@ -0,0 +1,2 @@
+# TODO: in addition to the existing behavior, it should probably also perspective warp to mimic
+# the directions that a person might hold the cards in their hard relative to the camera
